a8017e12835af3e4837a77625aae1b3030d66854
staku3 <csta19808@g.nihon-u.ac.jp>
2020-10-31 16:16:48 +0900

    LoginButtonとLoginFormを作成

---
+++ frontengine/src/components/LoginButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-button class="b-botton" @click="modalShow = !modalShow">{{loginText}}</b-button>
+
+    <b-modal v-model="modalShow"><LoginForm/></b-modal>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'LoginButton',
+  components: {
+    LoginForm
+  },
+  props: {
+    loginType: {
+      type: String,
+      default: 'login'
+    }
+  },
+  computed: {
+    loginText () {
+      if (this.loginType === 'login') {
+        return 'ログイン/登録'
+      } else {
+        return '未登録'
+      }
+    }
+  },
+  data () {
+    return {
+      modalShow: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.b-botton {
+  float: right;
+}
+
+</style>
--- frontengine/src/views/Home.vue
+++ frontengine/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <LoginButton loginType="login"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import LoginButton from '@/components/LoginButton.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginButton
   }
 }
 </script>
