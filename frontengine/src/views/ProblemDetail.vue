2ca4f45f8f6364a57b8550cabe4591f8bdbd219f
imamiya-masaki <imachan567@gmail.com>
2020-10-31 14:37:36 +0900

    テキストエリアと送信ボタンを作成

---
+++ frontengine/src/views/ProblemDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="problemdetail">
+      <b-form-textarea
+      id="textarea"
+      v-model="text"
+      placeholder="Enter something..."
+      rows="6"
+    ></b-form-textarea>
+    <b-button>送信</b-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
