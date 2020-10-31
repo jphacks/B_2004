b76ff14ceb891e4a1aee3c1bbe9a254150a484d6
Yu-k-29 <huyunisakuhananoyouni@gmail.com>
2020-10-31 15:13:27 +0900

    ヘッダーコンポーネントの作成　(デフォルト値)

--- frontengine/src/App.vue
+++ frontengine/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header></Header>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +8,16 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
