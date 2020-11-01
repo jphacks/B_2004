6cfaaf17d5ef586291aae252e2463d7c9b379284
imamiya-masaki <imamiyagit@gmail.com>
2020-11-02 00:48:06 +0900

    sumpleを設置できるようにする

--- frontengine/src/views/ProblemDetail.vue
+++ frontengine/src/views/ProblemDetail.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="problemdetail">
+    <b-button variant="outline-primary" @click="sumplePush()">サンプルを設置する</b-button>
       <b-form-textarea
       id="textarea"
       v-model="text"
       placeholder="Enter something..."
       rows="6"
     ></b-form-textarea>
-    <b-button @click="getDom(text)">送信</b-button>
+    <b-button @click="getDom()">送信</b-button>
   </div>
 </template>
 
@@ -23,8 +24,54 @@ export default {
     }
   },
   methods: {
-    getDom: function (text) {
-      MainProcess(text)
+    getDom: function () {
+      MainProcess(this.text)
+    },
+    sumplePush: function () {
+      this.text = this.getSumpleText
+    }
+  },
+  computed: {
+    getText () {
+      return "''"
+    },
+    getSumpleText () {
+      const output = []
+      output.push('<template>')
+      output.push('<div class="problemdetail">')
+      output.push('<b-form-textarea')
+      output.push('id="textarea"')
+      output.push('v-model="text"')
+      output.push('placeholder="Enter something..."')
+      output.push('rows="6"')
+      output.push('></b-form-textarea>')
+      output.push('<b-button @click="getDom(text)">送信</b-button>')
+      output.push('</div>')
+      output.push('</template>')
+      output.push('<script>')
+      output.push('import MainProcess from \'@/process/MainProcess.js\'')
+      output.push('export default {')
+      output.push('  name: \'Home\',')
+      output.push('  components: {')
+      output.push('  },')
+      output.push('  data () {')
+      output.push('    return {')
+      output.push('      text: \'\'')
+      output.push('    }')
+      output.push('  },')
+      output.push('  methods: {')
+      output.push('    getDom: function (text) {')
+      output.push('      MainProcess(text)')
+      output.push('    }')
+      output.push('  },')
+      output.push('  computed: {')
+      output.push('    getText () {')
+      output.push('      return "\'\'"')
+      output.push('    },')
+      output.push('  }')
+      output.push('}')
+      output.push('</ ' + 'script>')
+      return output.join('\n')
     }
   }
 }
