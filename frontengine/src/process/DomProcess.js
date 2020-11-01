7c2637c7d15d79d6890e0bd9625fe380c4940fec
imamiya-masaki <imamiyagit@gmail.com>
2020-11-02 03:04:46 +0900

    domtree作成

--- frontengine/src/process/DomProcess.js
+++ frontengine/src/process/DomProcess.js
@@ -1,10 +1,13 @@
 export default function (text) {
   const tags = []
   const tagCount = {} // そのタグが開かれている時true
+  const domTree = {}
+  const depths = {}
   // let hierarchy = 0
   // const err = []
   let unique = 0 // 各々のタグにuniqueIdを付与する
   let parentId = null
+  let depth = 0
   for (let i = 0; i < text.length; i++) {
     const mozi = text.charAt(i)
     if (mozi === '<') {
@@ -22,25 +25,42 @@ export default function (text) {
         if (!unique || !info) {
           console.log('check err?', info, unique)
         }
-        unique++
-        if (parentId && tags && tags[unique]) {
+        console.log('check err?::', parentId, info, unique, tags[unique])
+        if (parentId || parentId === 0) {
           console.log('parentId', parentId, tags, unique)
           tags[unique].parentId = parentId
         }
-        if (info.open) {
+        tags[unique].unique = unique
+        tags[unique].depth = depth
+        if (parentId) {
+          domTree[tags[unique].name] = {}
+        }
+        if (info.open && !info.close) {
           if (!tagCount[info.name]) {
             tagCount[info.name] = 0
           }
           tagCount[info.name]++
-          parentId = unique - 1 // 今代入したもの(++してないもの)を親とする
+          depth++
+          parentId = unique // 今代入したもの(++してないもの)を親とする
+          console.log('open処理', unique, tagCount, info)
         }
         if (info.close) {
           tagCount[info.name]--
+          tags[unique].depth--
+          depth--
+          parentId = tags[parentId].parentId
+          console.log('close処理', tagCount, info)
         }
+        if (!depths[tags[unique].depth]) {
+          depths[tags[unique].depth] = {}
+        }
+        depths[tags[unique].depth][unique] = tags[unique]
+        unique++
       }
     }
   }
-  console.log('file', text, text.charAt(1), text.length, tags)
+  const tree = createDomTree(depths)
+  console.log('file', text, text.charAt(1), text.length, tags, tree)
 }
 
 function DOMAnalysis (dom) {
@@ -190,4 +210,24 @@ function otherAnalysis (other) {
   }
   console.log('target', target)
   return target
+}
+
+function createDomTree (depths) {
+  const length = Object.keys(depths).length - 1
+  for (let i = length; i > 0; i--) {
+    for (const seed of Object.values(depths[i])) {
+      if (!seed.close && seed.parentId && seed.parentId >= 0) {
+        if (!depths[i - 1][seed.parentId].children) {
+          depths[i - 1][seed.parentId].children = {}
+        }
+        depths[i - 1][seed.parentId].children[seed.name] = seed
+      } else {
+      }
+    }
+  }
+  for (const seed of Object.values(depths[0])) {
+    if (!seed.close) {
+      return seed
+    }
+  }
 }
