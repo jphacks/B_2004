5523c7db74c0e1bc21fc3f25eb5b4a339928edf7
imamiya-masaki <imamiyagit@gmail.com>
2020-11-07 06:13:52 +0900

    整える

--- frontengine/functions/process/ScriptUtility/computedProcess.js
+++ frontengine/functions/process/ScriptUtility/computedProcess.js
@@ -3,6 +3,7 @@ export default function (body) {
   for (const property of body.value.properties) {
     output[property.key.name] = property.body
     if (!property.body) {
+      console.log('computed', property)
       continue
     } else {
 
---
+++ frontengine/functions/process/moduleProcess.js
@@ -0,0 +1,46 @@
+import propsProcess from './ScriptUtility/propsProcess.js'
+import dataProcess from './ScriptUtility/dataProcess.js'
+import methodsProcess from './ScriptUtility/methodsProcess.js'
+import computedProcess from './ScriptUtility/computedProcess.js'
+import { execScript } from './ScriptUtility/execScript.js'
+const global = {}
+export { global }
+export default function (ast) {
+  // console.log('ast', ast)
+  console.log('getkakuninn!!', ast.program.body)
+  const firstBody = ast.program.body[0].declaration.properties
+  const modules = {}
+  for (const body of firstBody) {
+    // console.log('body', body)
+    modules[body.key.name] = body
+  }
+  for (const key of Object.keys(modules)) {
+    switch (key) {
+      case 'props':
+        mergeObject(propsProcess(modules[key]))
+        break
+      case 'methods':
+        mergeObject(methodsProcess(modules[key]))
+        break
+      case 'data':
+        mergeObject(dataProcess(modules[key]))
+        break
+      case 'computed':
+        mergeObject(computedProcess(modules[key]))
+        break
+      case 'mounted':
+        break
+    }
+  }
+  // console.log('srhjeosije', global.text)
+  console.log('output!!!!!', global)
+  const output = execScript(global.testObject, global.text)
+  console.log('output!!!', output, global)
+  // console.log('moduleOutput', global, modules)
+  return modules
+}
+function mergeObject (obj) {
+  Object.keys(obj).forEach(key => {
+    global[key] = obj[key]
+  })
+}
