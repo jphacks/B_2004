41c7dc041b13390be8291cb5db2fe60bc8b2607b
Yu-k-29 <huyunisakuhananoyouni@gmail.com>
2020-11-06 10:26:40 +0900

    ユーザIDごとにコンテストの参加登録

--- frontengine/src/components/ProblemCard.vue
+++ frontengine/src/components/ProblemCard.vue
@@ -6,7 +6,8 @@
       </b-card-header>
       <b-card-body>
         <b-card-text>{{problemStatement}}</b-card-text>
-        <router-link @click="sss" :to="{name: 'ProblemDetail', params: {examId: problemId}}">参加登録</router-link>
+        <b-card-text>{{ getLoginId }}</b-card-text>
+        <router-link :to="{name: 'ProblemDetail', params: {examId: problemId}}" @click.native="chk()">参加登録</router-link>
       </b-card-body>
     </b-card>
   </div>
@@ -14,18 +15,29 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import firebase from 'firebase'
 export default {
   name: 'ProblemCard',
   props: {
     problemNumber: String,
     problemStatement: String,
     problemId: String,
-    exams: Object
+    exam: Object
   },
-  conputed: {
+  methods: {
+    chk () {
+      firebase.firestore().collection('users').doc(this.getLoginId).collection('join').doc(this.problemId).set({
+        difficult: this.exam.difficult,
+        name: this.exam.name
+        // startAt: this.exam.startAt
+      })
+    }
+  },
+  computed: {
     ...mapGetters(['getUserId']),
     getLoginId () {
-      return this.getLogId
+      console.log('check', this.getUserId)
+      return this.getUserId
     }
 
   }
