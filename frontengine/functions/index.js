ac6bab2da90a0d37587880201a36b134ddd60312
imamiya-masaki <imamiyagit@gmail.com>
2020-11-07 07:05:44 +0900

    import

--- frontengine/functions/index.js
+++ frontengine/functions/index.js
@@ -1,8 +1,10 @@
+import mainProcess from './process/MainProcess'
 const functions = require('firebase-functions');
 const items = 'tesutetuett';
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+const 
 exports.submitExam = functions.https.onCall((data, context) => {
     console.log('request', data, context);
     return data;
