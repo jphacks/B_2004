a66a8cd3df1542a965fc05cac638a9f363563c2c
imamiya-masaki <imamiyagit@gmail.com>
2020-11-16 03:59:37 +0900

    domをparseして表示できるように && vueコンポーネントを動的レンダリング

--- frontengine/src/components/preview/PreviewField.vue
+++ frontengine/src/components/preview/PreviewField.vue
@@ -2,7 +2,9 @@
   <div class="PreviewField">
     <b-button v-if="dom" @click="previewParse()">プレビューを表示する</b-button>
     <b-card>
-      <!-- {{ getDom }} -->
+      <div :id="'targetPreview'" class="targetPreviewFiled">
+
+      </div>
     </b-card>
   </div>
 </template>
@@ -10,7 +12,7 @@
 <script>
 // @ is an alias to /src
 import { domPreviewParse } from '@/process/ScriptUtility/domPreviewParse.js'
-
+import Vue from 'vue/dist/vue.esm.js'
 export default {
   name: 'PreviewField',
   components: {
@@ -19,19 +21,61 @@ export default {
     dom: {},
     inputScript: {}
   },
+  data () {
+    return {
+      outputDom: {},
+      pushPreview: ''
+    }
+  },
   computed: {
     getDom () {
       return this.dom
     }
   },
+  mounted: function () {
+    this.testPush()
+  },
   methods: {
-    domEvent: function (order, path, ...args) {
+    domEvent: function (order, path) {
       // domのfunction系を一旦ここに噛ませる
       // orderはfunction名
       // pathはcomponentファイル名
+      console.log('order', order, path)
+    },
+    classEvent: function (path, ...orders) {
+      // class名を受け取る
+      console.log('class', path, orders)
     },
     previewParse: function () {
-      domPreviewParse(this.dom)
+      this.outputDom = domPreviewParse(this.dom, 'default')
+      const self = this
+      const domEvent = this.domEvent
+      const classEvent = this.classEvent
+      const testSumple = '<div v-if="true" @click="domEvent()">sumplePush</div>'
+      let newPreviewDom = Vue.component('newPreviewDom', {
+        // data: function () {
+        //   // ここに対象のglobal入れる
+        //   return {
+        //     template: 20
+        //   }
+        // },
+        template: testSumple,
+        methods: {
+          domEvent: domEvent,
+          classEvent: classEvent
+        }
+      })
+      let vm = new Vue({
+        render: h => h(newPreviewDom)
+      })
+      // this.pushPreview = newPreviewDom
+      console.log('check', newPreviewDom, this.vue, Vue)
+      vm.$mount(document.getElementById('targetPreview'))
+    },
+    testPush: function () {
+      console.log('document', document, this.document)
+      const sumpleText = [`<div> aaaa </div>`]
+      return sumpleText
     }
   }
 }
--- frontengine/src/process/ScriptUtility/domPreviewParse.js
+++ frontengine/src/process/ScriptUtility/domPreviewParse.js
@@ -6,22 +6,100 @@ function domPreviewParse (domTree, fileName) {
   while (stack.length > 0) {
     const take = stack.pop()
     const parseDom = {}
-    parseDom.class = take.class.variables.join(' ')
+    if (take.hasOwnProperty('class')) {
+      const targetDom = []
+      targetDom.push(fileName)
+      if (take.class.hasOwnProperty('variables')) {
+        targetDom.push(...take.class.variables)
+      }
+      parseDom['v-bind:style'] = 'this.classEvent(' + targetDom.join(',') + ')'
+    }
     if (take.hasOwnProperty('v-for')) {
       const targetValue = []
       if (take['v-for'].target.hasOwnProperty('value')) {
-        targetValue.push(take.target.value)
+        targetValue.push(take['v-for'].target.value)
       }
       if (take['v-for'].target.hasOwnProperty('index')) {
-        targetValue.push(take.target.index)
+        targetValue.push(take['v-for'].target.index)
       }
       let targetInput = ''
-      if (take['v-for'].type === 'function') {
-        const targetArg = [...take['v-for'].functionArgument]
-      } else if (take['v-for'].type === 'variable') {
-
+      const targetDom = []
+      if (take['v-for'].type) {
+        targetDom.push(take['v-for'].right)
+        targetDom.push(fileName)
+        targetInput = 'this.domEvent(' + targetDom.join(',') + ')'
       }
       // const targetOutput = '(' + targetValue.join(',') + ')' + ' of ' +
+      parseDom['v-for'] = '(' + targetValue.join(',') + ') of ' + targetInput
     }
+    if (take.hasOwnProperty('others')) {
+      // 現状v-forとclassを分けたらいけるか...?
+      for (let i = 0; i < take.others.length; i++) {
+        if (take.others[i].left === 'v-for' || take.others[i].left === 'class') {
+          continue
+        }
+        const key = take.others[i].left
+        let targetInput = ''
+        const targetDom = []
+        if (take.others[i].type) {
+          targetDom.push(take.others[i].right)
+          targetDom.push(fileName)
+          targetInput = 'this.domEvent(' + targetDom.join(',') + ')'
+        }
+        parseDom[key] = targetInput
+      }
+    }
+    if (take.name === 'reserveText' && take.reserves) {
+      const textOutput = []
+      for (let i = 0; i < take.reserves.length; i++) {
+        const reserveVal = take.reserves[i]
+        if (reserveVal.type === 'direct') {
+          textOutput.push(reserveVal.textRawValue)
+        } else {
+          const targetDom = []
+          targetDom.push(reserveVal.textRawValue)
+          targetDom.push(fileName)
+          textOutput.push('{{ this.domEvent(' + targetDom.join(',') + ') }}')
+        }
+      }
+      parseDom.reserveText = textOutput.join('')
+    }
+    // --各々の作用
+    if (!parseDom.hasOwnProperty('reserveText')) {
+      const pushOutput = []
+      for (let i = 0; i < Object.keys(parseDom).length; i++) {
+        const key = Object.keys(parseDom)[i]
+        pushOutput.push(key + '="' + parseDom[key] + '"')
+      }
+      let endBlock = '>'
+      if (!take.open && !take.enClose) {
+        endBlock = '/>'
+      }
+      let startBlock = '<'
+      if (take.enClose) {
+        startBlock = '</'
+      }
+      output.push(startBlock + take.name + ' ' + pushOutput.join(' ') + endBlock)
+    } else {
+      output.push(parseDom.reserveText)
+    }
+    // --parseしてpush
+
+    if (take.open) {
+      const enCloseTag = {}
+      enCloseTag.name = take.name
+      enCloseTag.enClose = true
+      stack.push(enCloseTag)
+      if (take.hasOwnProperty('children')) {
+        for (let i = take.children.length - 1; i >= 0; i--) {
+          const childVal = take.children[i]
+          stack.push(childVal)
+        }
+      }
+    }
+    // --子供に対する作用
+    // --whileEnd
   }
+  console.log('output', output)
+  return output.join('')
 }
