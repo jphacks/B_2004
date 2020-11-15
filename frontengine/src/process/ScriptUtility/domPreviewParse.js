e1550ceb0ce12490824e04b83c8bb0d0a766f4db
imamiya-masaki <imamiyagit@gmail.com>
2020-11-15 15:51:40 +0900

    classのやつがmergeされてないな

--- frontengine/src/process/ScriptUtility/domPreviewParse.js
+++ frontengine/src/process/ScriptUtility/domPreviewParse.js
@@ -1,4 +1,10 @@
 export { domPreviewParse }
 function domPreviewParse (domTree) {
   console.log('getDomPreviewParse', domTree)
+  const output = []
+  let stack = [domTree]
+  while (stack.length > 0) {
+    const take = stack.pop()
+    const parseDom = take
+  }
 }
