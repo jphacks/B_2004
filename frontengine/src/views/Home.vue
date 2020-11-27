920c19b2d036926debb86cdbfa5b23d9bdf54358
staku3 <csta19808@g.nihon-u.ac.jp>
2020-11-28 08:16:35 +0900

    HomeとList直しました

--- frontengine/src/views/Home.vue
+++ frontengine/src/views/Home.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="home">
     <img alt="frontEngine logo" src="../assets/frontEngineLogo.png">
-    <b-button @click="testSakai()">テスト5（坂井）</b-button>
     <HomeDesign msg="frontEngineへようこそ"/>
-    <b-card><exam-4 :input="input"/></b-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomeDesign from '@/components/HomeDesign.vue'
-import Exam4 from '@/components/Exam4.vue'
 
 export default {
   name: 'Home',
   components: {
-    HomeDesign,
-    Exam4
-  },
-  methods: {
-    testSakai: function () {
-      this.$router.push({
-        name: "Exam5"
-      })
-    },
-    getter: function (params) {
-      console.log('params', params)
-    }
+    HomeDesign
   },
   data () {
     return {
